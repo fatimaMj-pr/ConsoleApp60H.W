8af7365c5e5933dd792a221c58155fdaf9e6ee2b
fatimaMj-pr <fm1425798@gmail.com>
2024-11-15 18:11:36 +0300

    Add project files.

---
+++ ConsoleApp60/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.W
+{ internal class vehicle
+    {
+        public string Brand { get; set; }
+    }
+    class MotorVehicle : vehicle
+    {
+        public string ModelName { get; set; }
+    }
+    class car : MotorVehicle
+    {
+        public car(string brand, string modelName)
+        {
+            Brand = brand;
+            ModelName = modelName;
+        }
+        public void DisplaycarInfo()
+        {
+            Console.WriteLine("car Brand:" + Brand + "  " + " Model:" + ModelName);
+        }
+
+    }
+
+    class Truck : MotorVehicle
+    {
+        public Truck(string brand, string modelName)
+        {
+            Brand = brand;
+            ModelName = modelName;
+        }
+        public void DisplayTruckInfo()
+        {
+
+            Console.WriteLine("Truck Brand:" + Brand + "  " + "Model:" + ModelName);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car mycar = new car("BMW", "c400");
+            Truck myTruck = new Truck("Mercedes", "v11");
+            mycar.DisplaycarInfo();
+            myTruck.DisplayTruckInfo();
+            Console.ReadKey();
+
+
+        }
+    }
+}
